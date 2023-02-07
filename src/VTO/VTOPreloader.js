6c16423e109c5d189577f603f749a4bcee4af6ae
Min GuoQiang <minguoqiang88@outlook.com>
2023-02-07 16:06:04 +0300

    implement preloader and styling

--- src/VTO/VTOPreloader.js
+++ src/VTO/VTOPreloader.js
@@ -1,11 +1,22 @@
 import PropTypes from "prop-types";
+import { VTOEywaIcon } from "./VTOEywaIcon";
 import "./VTOPreloader.css";
 
 const VTOPreloader = ({ progress, isLoading }) => {
   return (
     <>
       <div className={`vto-loader ${isLoading ? "" : "display-none"}`}>
-        Progress... {progress}
+        <div className="vto-splash-loader">
+          <div>
+            <VTOEywaIcon className={""} />
+          </div>
+          <p>Virtual experience is loading...</p>
+          <div className="vto-progress-wrapper"></div>
+          <div className="vto-blur-text">
+            We are committed to protecting your privacy. We use the information
+            provided to personalise your experience and not store any data
+          </div>
+        </div>
       </div>
     </>
   );
